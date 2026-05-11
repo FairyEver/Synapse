23a560ada66e8dc0ce859046a9f4e8bb9181bd20
liy <1711467488@qq.com>
2026-05-11 20:45:02 +0800

    feat(workflow): add switch node layout constants

---
+++ desktop/workflow-nodes/switch/constants.ts
@@ -0,0 +1,5 @@
+/** Height of the switch card header section (icon + name + agent info). */
+export const SWITCH_HEADER_H = 56
+
+/** Height of each branch row inside the switch card. */
+export const SWITCH_BRANCH_H = 28
