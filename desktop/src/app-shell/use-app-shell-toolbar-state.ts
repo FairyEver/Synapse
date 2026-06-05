348829d0ed79c81cc0d83079be29153d9188c76e
liy <1711467488@qq.com>
2026-06-05 22:32:55 +0800

    fix: resolve issue #1209

--- desktop/src/app-shell/use-app-shell-toolbar-state.ts
+++
@@ -1,2 +0,0 @@
-export { useRepositoryToolbarState as useAppShellToolbarState } from "@/app-shell/use-repository-toolbar-state"
-export type { RepositoryToolbarState as AppShellToolbarState } from "@/app-shell/use-repository-toolbar-state"
