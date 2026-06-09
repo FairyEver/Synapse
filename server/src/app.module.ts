d0ad06d47eb1a80fcfd8bc3f234740fdf82b121e
liy <1711467488@qq.com>
2026-06-09 23:31:07 +0800

    feat(server): register content store module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,6 +8,7 @@ import { AdminModule } from "./admin/admin.module"
 import { AdminAuthModule } from "./admin-auth/admin-auth.module"
 import { UserAuthModule } from "./auth/user-auth.module"
 import { BackupModule } from "./backup/backup.module"
+import { ContentStoreModule } from "./content-store/content-store.module"
 import { DashboardModule } from "./dashboard/dashboard.module"
 import { DriveModule } from "./drive/drive.module"
 import { HealthModule } from "./health/health.module"
@@ -67,6 +68,7 @@ type RequestLogObject = {
     WebhookModule,
     AdminAuthModule,
     DashboardModule,
+    ContentStoreModule,
     DriveModule,
     AdminModule,
     BackupModule,
