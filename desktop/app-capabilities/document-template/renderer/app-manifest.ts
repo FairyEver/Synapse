eb42a6efcd6841a2f6aaaae157ef88f2025d89cd
liy <1711467488@qq.com>
2026-06-23 18:33:18 +0800

    chore(document-template): use custom app icon

--- desktop/app-capabilities/document-template/renderer/app-manifest.ts
+++ desktop/app-capabilities/document-template/renderer/app-manifest.ts
@@ -1,6 +1,6 @@
 import type { SynapseSystemAppManifest } from "../../../src/modules/apps/types"
 import { documentTemplateAppDefinition } from "./app-definition"
-import icon from "./icon.png"
+import icon from "./assets/icon.png"
 
 export const documentTemplateAppManifest = {
   ...documentTemplateAppDefinition,
