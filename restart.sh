57969dc3c07b5da70868b067ffd13be93915c14c
liy <1711467488@qq.com>
2026-08-01 09:30:35 +0800

    fix(server): serve admin root from admin bundle

--- restart.sh
+++ restart.sh
@@ -142,6 +142,7 @@ check_not_redirect_to_dashboard() {
 
 check_body_contains "healthz" "http://127.0.0.1:3000/healthz" '"status":"ok"'
 check_body_contains "console" "http://127.0.0.1:3000/console/" '<div id="root">'
+check_body_contains "admin" "http://127.0.0.1:3000/admin/" '<title>Synapse 管理</title>'
 check_redirect "dashboard redirect" "http://127.0.0.1:3000/dashboard" "/console/"
 check_not_redirect_to_dashboard "webhook route" "http://127.0.0.1:3000/webhooks/not-found/test"
 check_not_redirect_to_dashboard "drive share route" "http://127.0.0.1:3000/share/shr_not_found"
